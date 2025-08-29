239c55ea9146fc32a2817d469f6f051eb83a1611
rsshonjoydas <rsshonjoydas@gamil.com>
2025-08-29 21:25:38 +0600

    nestjs: Inject the Service into the Controller

--- 03-creating-rest-api/src/modules/songs/songs.controller.ts
+++ 03-creating-rest-api/src/modules/songs/songs.controller.ts
@@ -1,28 +1,18 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+
+import { SongsService } from './songs.service';
 
 @Controller('songs')
 export class SongsController {
+  constructor(private songsService: SongsService) {}
+
   @Post()
   create() {
-    return 'create a new song endpoint';
+    return this.songsService.create('Animals by Martin Garrix');
   }
 
   @Get()
   findAll() {
-    return 'find all songs endpoint';
-  }
-  @Get(':id')
-  find() {
-    return 'fetch song on the based on id';
-  }
-
-  @Put(':id')
-  update() {
-    return 'update song on the based on id';
-  }
-
-  @Delete(':id')
-  delete() {
-    return 'delete song on the based on id';
+    return this.songsService.findAll();
   }
 }
